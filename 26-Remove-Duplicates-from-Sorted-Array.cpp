6f9c7db172a5f12fbd346ac1c6aa8ec5cb5e1e21
ARPIT SINGH <adoranto737@gmail.com>
2025-01-10 00:33:10 +0530

    [26] [Time Beats: 100.00%] [Memory Beats: 15.39%] - LeetPush

---
+++ 26-Remove-Duplicates-from-Sorted-Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& arr) {
+        int n=arr.size();
+        int i = 0;
+        for (int j = 1; j < n; j++) {
+            if (arr[i] != arr[j]) {
+            arr[++i] = arr[j];
+            }
+        }
+        return i + 1;
+    }
+};
