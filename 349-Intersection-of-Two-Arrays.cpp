4dca91d558673cf5acb79a6d3f6cf5294c471e0d
ARPIT SINGH <adoranto737@gmail.com>
2025-01-12 11:54:36 +0530

    [349] [Time Beats: 21.80%] [Memory Beats: 46.19%] - LeetPush

---
+++ 349-Intersection-of-Two-Arrays.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& a, vector<int>& b) {
+        unordered_set<int> sa(a.begin(), a.end());  
+        vector<int> res;                            
+        for (int i = 0; i < b.size(); i++) {
+            if (sa.find(b[i]) != sa.end()) {
+                res.push_back(b[i]); 
+                sa.erase(b[i]);  
+            }
+        }
+        return res;
+    }
+};
