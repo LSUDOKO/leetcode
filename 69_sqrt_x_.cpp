952e6ce9f50fac87bbcadbfb53802c6c87aa369a
ARPIT SINGH <adoranto737@gmail.com>
2025-05-15 16:19:45 +0530

    Update 69_sqrt_x_.cpp - LeetCode Problem 69

--- 69_sqrt_x_.cpp
+++ 69_sqrt_x_.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
-    int mySqrt(int x) {
-        int ans = sqrt(x);
-        return ans;
+    int mySqrt(int n) {
+        long  low = 1, high = n;
+        while (low <= high) {
+            long long mid = (low + high) / 2;
+            long long square = mid * mid;
+
+            if (square <= n) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return high;
     }
 };
