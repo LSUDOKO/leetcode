112c6b97e6f51e5aaa8ba3245ff5acdeaea98709
Arpit Singh <adoranto737@gmail.com>
2025-09-13 18:47:47 +0530

    Add 957_minimum_add_to_make_parentheses_valid.cpp - LeetCode Problem 957

---
+++ 957_minimum_add_to_make_parentheses_valid.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<int> st;
+        int count=0;
+        for (char c:s){
+            if (c=='('){
+                st.push(c);
+            }
+            else{
+                if(!st.empty()){
+                    st.pop();
+                }
+                else {
+                    count++;
+                }
+            }
+        }
+        return count+st.size();
+        
+    }
+};
