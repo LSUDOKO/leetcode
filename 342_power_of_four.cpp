fffddf079daff6dea16da6989c98ecfb21108717
Arpit Singh <adoranto737@gmail.com>
2025-09-25 19:16:15 +0530

    Update 342_power_of_four.cpp - LeetCode Problem 342

--- 342_power_of_four.cpp
+++ 342_power_of_four.cpp
@@ -17,15 +17,22 @@ public:
     // return n==1;}//timexomplexity log4(n)
 
     //Approach 3
-    if (n<=0){
-        return false;
-    }
-    int x=log(n)/log(4);
-    if (pow(4,x)==n){
+    // if (n<=0){
+    //     return false;
+    // }
+    // int x=log(n)/log(4);
+    // if (pow(4,x)==n){
+    //     return true;
+    // }
+    // else {
+    //     return false;
+    // }
+    // 
+    //log(n)+log(x)
+    //approch 4
+    if (n <= 0) return false;
+    if (((n & (n - 1)) == 0) && ((n - 1) % 3 == 0)) {
         return true;
     }
-    else {
-        return false;
-    }
-    }
+    return false;}
 };
