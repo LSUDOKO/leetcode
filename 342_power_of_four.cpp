25acc0e690a5f44278e908e7ddaa1bd4ee3e62c7
Arpit Singh <adoranto737@gmail.com>
2025-09-25 19:04:47 +0530

    Update 342_power_of_four.cpp - LeetCode Problem 342

--- 342_power_of_four.cpp
+++ 342_power_of_four.cpp
@@ -1,8 +1,31 @@
 class Solution {
 public:
+    //AProch 1
     bool isPowerOfFour(int n) {
-        if (n <= 0) return false;
-    if ((n & (n - 1)) != 0) return false;
-    return (n & 0x55555555) != 0;
+    //     if (n <= 0) return false;
+    // if ((n & (n - 1)) != 0) return false;
+    // return (n & 0x55555555) != 0;
+    // }
+
+    //AProch 2
+    // while(n>1){
+    //     if (n%4!=0){
+    //         return false;
+    //     }
+    //     n=n/4;
+    // }
+    // return n==1;}//timexomplexity log4(n)
+
+    //Approach 3
+    if (n<=0){
+        return false;
+    }
+    int x=log(n)/log(4);
+    if (pow(4,x)==n){
+        return true;
+    }
+    else {
+        return false;
+    }
     }
 };
