1f0a624a4c4e2e9064469d4f24b5c08b46410ce3
Arpit Singh <adoranto737@gmail.com>
2025-09-30 23:05:03 +0530

    Update 2324_find_triangular_sum_of_an_array.cpp - LeetCode Problem 2324

--- 2324_find_triangular_sum_of_an_array.cpp
+++ 2324_find_triangular_sum_of_an_array.cpp
@@ -13,7 +13,7 @@ public:
 
     int n=nums.size();
     for (int size=n-1;size>=1;size--){
-        for (int i=0;i<n;i++){
+        for (int i=0;i<size;i++){
             nums[i]=(nums[i]+nums[i+1])%10;
         }
     }
