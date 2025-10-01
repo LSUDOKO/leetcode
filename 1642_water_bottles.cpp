47f39cd8c9c63a9e23801522ea4c3bb72da06bb1
Arpit Singh <adoranto737@gmail.com>
2025-10-01 17:41:52 +0530

    Add 1642_water_bottles.cpp - LeetCode Problem 1642

---
+++ 1642_water_bottles.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        // long long  sum=numBottles;
+        // long long rem=0;
+        // while(numBottles>0){
+        //     int a=(numBottles/numExchange);
+        //     rem+=(numBottles%numExchange);
+        //     if (rem>=numExchange){
+        //         sum+=rem/numExchange;
+        //         numBottles++;
+        //         rem=rem%numExchange;
+        //     }
+        //     // if (a==0){
+        //     //     sum++;
+        //     // }
+        //     sum+=a;
+        //     numBottles=numBottles/numExchange;
+        // }
+        // return sum;
+        long long sum = numBottles;
+        long long rem = 0;
+        while (numBottles > 0) {
+            int a = numBottles / numExchange;
+            rem += (numBottles % numExchange);
+
+            int extra = rem / numExchange;
+            rem = rem % numExchange;
+
+            sum += a + extra;                         
+            numBottles = a + extra;                   
+        }
+        return (int)sum;
+    }
+};
