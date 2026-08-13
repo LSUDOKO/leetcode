440b34b5183efc52557217c9849761d1e237fa8c
Arpit Singh <adoranto737@gmail.com>
2026-08-13 19:14:02 +0530

    2213 Longest Substring of One Repeating Character - Runtime: 123 ms, Memory: 101 MB

---
+++ 2213-longest-substring-of-one-repeating-character.cpp
@@ -0,0 +1,81 @@
+class Solution {
+public:
+    struct Node {
+        int pre = 0;        // longest same-letter run starting at the left edge
+        int suf = 0;        // longest same-letter run ending at the right edge
+        int maxLen = 0;     // longest same-letter run anywhere inside
+        char leftChar = 0;  // letter at the left edge
+        char rightChar = 0; // letter at the right edge
+    };
+
+    int n;
+    vector<Node> tree; //segmen tree size 4*n
+
+    Node merge(const Node& L, const Node& R, int leftLen, int rightLen) {
+        Node res;
+
+        res.leftChar  = L.leftChar;
+        res.rightChar = R.rightChar;
+
+        res.pre = L.pre;
+        if (L.pre == leftLen && L.rightChar == R.leftChar) {
+            res.pre = L.pre + R.pre;
+        }
+
+        res.suf = R.suf;
+        if (R.suf == rightLen && L.rightChar == R.leftChar) {
+            res.suf = R.suf + L.suf;
+        }
+
+        res.maxLen = max(L.maxLen, R.maxLen);
+        if (L.rightChar == R.leftChar) {
+            res.maxLen = max(res.maxLen, L.suf + R.pre);
+        }
+
+        return res;
+    }
+
+    void buildSegmentTree(int i, int l, int r, string& s) {
+        if (l == r) {
+            tree[i] = { 1, 1, 1, s[l], s[l] };
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        buildSegmentTree(2 * i + 1, l, mid, s);
+        buildSegmentTree(2 * i + 2, mid + 1, r, s);
+        tree[i] = merge(tree[2 * i + 1], tree[2 * i + 2], mid - l + 1, r - mid);
+    }
+
+    void update(int i, int l, int r, int pos, char ch) {
+        if (l == r) { //l == r == pos
+            tree[i] = { 1, 1, 1, ch, ch };
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        if (pos <= mid) {
+            update(2 * i + 1, l, mid, pos, ch);
+        } else {
+            update(2 * i + 2, mid + 1, r, pos, ch);
+        }
+        tree[i] = merge(tree[2 * i + 1], tree[2 * i + 2], mid - l + 1, r - mid);
+    }
+
+    vector<int> longestRepeating(string s, string queryCharacters, vector<int>& queryIndices) {
+        n = s.size();
+        tree.assign(4 * n, Node()); //segmen tree size 4*n
+
+        buildSegmentTree(0, 0, n - 1, s);
+
+        int k = queryIndices.size();
+
+        vector<int> result(k);
+        for (int i = 0; i < k; i++) {
+            int pos = queryIndices[i];
+            char ch = queryCharacters[i];
+            update(0, 0, n - 1, pos, ch);
+            result[i] = tree[0].maxLen; //root node covers entire string
+        }
+
+        return result;
+    }
+};
