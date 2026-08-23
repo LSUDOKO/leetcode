fe40b982264554b8681c8d7b349e2dd652b3f792
Arpit Singh <adoranto737@gmail.com>
2026-08-23 21:46:29 +0530

    783 Minimum Distance Between BST Nodes - Runtime: 0 ms, Memory: 12.5 MB

---
+++ 783-minimum-distance-between-bst-nodes.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    TreeNode* prev=nullptr;
+public:
+    void helper(TreeNode* root,int &mini){
+        if (root==NULL){
+            return ;
+        }
+        helper(root->left,mini);
+        if (prev!=nullptr){
+            mini=min(mini,abs(prev->val-root->val));
+        }
+        prev=root;
+        helper(root->right,mini);
+    }
+    int minDiffInBST(TreeNode* root) {
+        int mini=INT_MAX;
+        helper(root,mini);
+        return mini;
+
+    }
+};
