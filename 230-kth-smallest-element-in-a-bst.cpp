ec95298fbaff44780b92a321afa288dc4cd49d1a
Arpit Singh <adoranto737@gmail.com>
2026-08-23 22:50:17 +0530

    230 Kth Smallest Element in a BST - Runtime: 0 ms, Memory: 24.5 MB

---
+++ 230-kth-smallest-element-in-a-bst.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    vector<int>ans;
+public:
+    void helper(TreeNode* root,int k){
+        if (root==NULL){
+            return;
+        }
+        helper(root->left,k);
+        ans.push_back(root->val);
+        helper(root->right,k);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        helper(root,k);
+        return ans[k-1];
+    }
+};
