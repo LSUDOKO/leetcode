9e70204e98ba9e26cf2a051c87e4b1f2bb65e907
Arpit Singh <adoranto737@gmail.com>
2026-09-04 21:44:07 +0530

    3903 Smallest Stable Index I - Runtime: 1 ms, Memory: 31.5 MB

---
+++ 3903-smallest-stable-index-i.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<int>maxi(n);
+        vector<int>mini(n);
+        int maximum=INT_MIN;
+        int minimum=INT_MAX;
+        for (int i=0;i<n;i++){
+            if(maximum>nums[i]){
+                maxi[i]=maximum;
+
+            }
+            else{
+                maximum=nums[i];
+                maxi[i]=maximum;
+            }
+        }
+        for (int i=n-1;i>=0;i--){
+            if(minimum<nums[i]){
+                mini[i]=minimum;
+
+            }
+            else{
+                minimum=nums[i];
+                mini[i]=minimum;
+            }
+        }
+
+        for (int i=0;i<n;i++){
+            if (maxi[i]-mini[i]<=k){
+                return i;
+            }
+        }
+        return -1;
+
+    }
+};
