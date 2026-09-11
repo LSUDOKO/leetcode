e0c77d7b1ee881c51d532c2419ff0539769d57ed
Arpit Singh <adoranto737@gmail.com>
2026-09-11 16:16:47 +0530

    56 Merge Intervals - Runtime: 7 ms, Memory: 23.7 MB

---
+++ 56-merge-intervals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n=intervals.size();
+        sort(intervals.begin(),intervals.end());
+        int l=intervals[0][0];
+        int r=intervals[0][1];
+        vector<vector<int>>ans;
+        for (int i=1;i<n;i++){
+            if(intervals[i][0]<=r){
+                r=max(r,intervals[i][1]);
+            }else{
+                ans.push_back({l,r});
+                l=intervals[i][0];
+                r=intervals[i][1];
+            }
+        }
+        ans.push_back({l,r});
+        return ans;
+    }
+};
